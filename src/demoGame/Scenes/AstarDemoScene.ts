999d17a6f5cf7c3907e7c668164f2a95d5b4616d
lixingliu <lixing.liu@stonybrook.edu>
2023-04-07 23:15:10 -0400

    TEST

--- src/demoGame/Scenes/AstarDemoScene.ts
+++ src/demoGame/Scenes/AstarDemoScene.ts
@@ -47,7 +47,7 @@ export default class AStarDemoScene extends Scene {
 
         
         // TODO Set the navigation strategy to be A*
-        navmesh.setStrategy("direct");
+        navmesh.setStrategy("astar");
 
         // Create a dummy NPC
         this.npc = this.add.animatedSprite(NPCActor, "BlueEnemy", "primary")
